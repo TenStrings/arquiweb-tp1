716bc3faa88847520b223349260862ea97447b9a
Enzo Cioppettini <ecioppettini@atixlabs.com>
2019-04-25 23:17:55 -0300

    Polish a la mockdata

--- src/api.js
+++ src/api.js
@@ -1,12 +1,22 @@
 const categoriesMock = [
-    { title: "category1", icon: "idk1" },
-    { title: "category2", icon: "idk2" },
-    { title: "category3", icon: "idk3" },
+    { id: 1, title: "Food", icon: "idk1" },
+    { id: 2, title: "Night life", icon: "idk2" },
+    { id: 3, title: "Sports", icon: "idk3" },
 ]
 
 const pointsMock = [
-    {id: 1, position: { lat: -34.583192397850446, lng: -58.44321326835347 }},
-    {id: 2, position: { lat: -34.57301606452501, lng: -58.454889807030376 }}
+    {
+        id: 1,
+        position: { lat: -34.583192397850446, lng: -58.44321326835347 },
+        name: "RamenHut",
+        category: 1
+    },
+    {
+        id: 2,
+        position: { lat: -34.57301606452501, lng: -58.454889807030376 },
+        name: "FutureBar",
+        category: 2
+    }
 ]
 
 export const categoriesAPI = {
