d3a16c64f1670eee81bae6ba424091299f28b170
tomasPastore <tpastore@dc.uba.ar>
2019-04-29 16:02:09 -0300

    agregue un par de pavadas tmb de diseño

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,9 @@ import MainMap from '../components/MainMap';
 import Categories from '../components/Categories';
 import POIFilter from '../components/POIFilter'
 
-import { Row, Col, Menu, Button, Dropdown, Card, Tag } from 'antd';
+import { Row, Col, Menu, Button, Dropdown, Card, Tag, Layout, Icon } from 'antd';
 import Jumbotron from 'react-bootstrap/Jumbotron'
+
 
 
 import { categoriesAPI, poiAPI } from '../api';
@@ -82,6 +83,10 @@ class Home extends Component {
   }
 
   render() {
+    const {
+      Header, Footer, Sider, Content,
+    } = Layout;
+
     const { poi, poiFilter } = this.state
     const poiMarkers =
       (poiFilter ? poiFilter(poi) : poi)
@@ -98,13 +103,13 @@ class Home extends Component {
     const menu_categories =
       <Menu>
         <Menu.Item>
-          <a target="_blank" rel="noopener noreferrer" href="http://www.youporn.com/">Category 1</a>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.dc.uba.ar/">Category 1</a>
         </Menu.Item>
         <Menu.Item>
-          <a target="_blank" rel="noopener noreferrer" href="http://www.xvideos.com/">Category 2</a>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.dc.uba.ar/">Category 2</a>
         </Menu.Item>
         <Menu.Item>
-          <a target="_blank" rel="noopener noreferrer" href="http://www.pornhub.com/">Category 3</a>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.dc.uba.ar/">Category 3</a>
         </Menu.Item>
       </Menu>
 
@@ -115,52 +120,77 @@ class Home extends Component {
     //<span className="ant-divider" style={{ margin: '0 1em' }} />
     return (
       <div className="App">
-        <Row style={{ background: '#ECECEC', padding: '20px' }}>
-          <h1> Customize your search </h1>
-        </Row>
-        <Row style={{ background: '#ECECEC'}}> <hr className="my-2" /> </Row>
-        <Row style={{ background: '#ECECEC', padding: '20px' }}  type="flex" >
-          <Col offset={1}>
-            <Card
-             title="Match if name starts with..."
-             style={{ width: 400 }}
-            >
-              <POIFilter onChange={this.handlePOIFilterChange} poi={poi}></POIFilter>
-            </Card>
+          <Layout>
+            <Sider style={{ background: '#FFFFFF'}} collapsible="true" collapsedWidth="0" span={10}>
+              <Categories data={this.state.categories} />
+            </Sider>
+            <Content>
+            <Row id="Filtros" style={{ background: '#ECECEC', padding: '20px' }}  type="flex" >
+              <Col offset={1}>
+                <Card
+                 title="Match if name starts with..."
+                 style={{ width: 300 }}
+                >
+                  <POIFilter onChange={this.handlePOIFilterChange} poi={poi}></POIFilter>
+                </Card>
+              </Col>
+              <Col offset={1}>
+                <Card
+                 title="Match if belongs to any of the listed categories."
+                 style={{ width: 500 }}
+                >
+                  <Tag closable onClose={log} color="magenta">Peluquerias</Tag>
+                  <Tag closable onClose={log} color="red">Boliches</Tag>
+                  <Tag closable onClose={log} color="volcano">Deportes</Tag>
+                  <Tag closable onClose={log} color="orange">orange</Tag>
+                  <Tag closable onClose={log} color="gold">gold</Tag>
+                  <Tag closable onClose={log} color="lime">lime</Tag>
+                  <Tag closable onClose={log} color="green">green</Tag>
+                  <Tag closable onClose={log} color="cyan">cyan</Tag>
+                  <Tag closable onClose={log} color="blue">blue</Tag>
+                  <Tag closable onClose={log} color="geekblue">geekblue</Tag>
+                  <Tag closable onClose={log} color="purple">purple</Tag>
+                  <br /> <br />
+                  <Row type="flex" justify="center">
+                    <Dropdown overlay={menu_categories} placement="bottomLeft">
+                      <Button type="primary" shape="circle" justify="center" icon="plus"></Button>
+                    </Dropdown>
+                  </Row>
+                </Card>
+
+              </Col>
+            </Row>
+            <Row style={{ background: '#ECECEC'}}> <hr className="my-2" /> </Row>
+
+            <Row id="Mapa">
+              <Col>
+                <MainMap
+                  style={{ height: '400px' }}
+                  mapCenter={this.state.mapCenter} markers={markers}
+                  onClick={this.onClick}
+                />
+              </Col>
+            </Row>
+            </Content>
+          </Layout>
+          <Footer>Footer</Footer>
+
+      <Row id="Row_1" type="flex" style={{padding: '20px' }} >
+
+        <Col id="Col_1" span={4}>
+
+        </Col>
+        <Col id="Col_2" span={15}>
+
+
+
+
           </Col>
-          <Col offset={1}>
-            <Card
-             title="Match if belongs to any of the listed categories."
-             style={{ width: 400 }}
-            >
-              <Dropdown overlay={menu_categories} placement="bottomLeft">
-                <Button>Add category to list</Button>
-              </Dropdown>
-              <br /> <br />
-              <Tag closable onClose={log} color="magenta">Peluquerias</Tag>
-              <Tag closable onClose={log} color="red">Boliches</Tag>
-              <Tag closable onClose={log} color="volcano">Deportes</Tag>
-              <Tag closable onClose={log} color="orange">orange</Tag>
-              <Tag closable onClose={log} color="gold">gold</Tag>
-              <Tag closable onClose={log} color="lime">lime</Tag>
-              <Tag closable onClose={log} color="green">green</Tag>
-              <Tag closable onClose={log} color="cyan">cyan</Tag>
-              <Tag closable onClose={log} color="blue">blue</Tag>
-              <Tag closable onClose={log} color="geekblue">geekblue</Tag>
-              <Tag closable onClose={log} color="purple">purple</Tag>
-            </Card>
-          </Col>
+
         </Row>
 
-        <Row>
-          <Col>
-            <MainMap
-              style={{ height: '400px' }}
-              mapCenter={this.state.mapCenter} markers={markers}
-              onClick={this.onClick}
-            />
-          </Col>
-        </Row>
+
+
 
         <form onSubmit={this.handleCategorySubmit}>
           <input type="text" name="categoryName" placeholder="Category name" />
